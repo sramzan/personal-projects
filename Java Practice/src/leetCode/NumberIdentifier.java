dd058c1cbca093bac4513b1788806f563fdcdad5
Sean Ramzan <sramzan16@gmail.com>
2016-01-23 23:26:25 -0500

    Attempt at finding numerical values in a string. - Using a byte code implementation.

--- Java Practice/src/leetCode/NumberIdentifier.java
+++ Java Practice/src/leetCode/NumberIdentifier.java
@@ -3,8 +3,24 @@ package leetCode;
 public class NumberIdentifier {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String test = "9";
+		byte[] barr = test.getBytes();
+		for (byte b : barr)
+			System.out.println(b);
+		
+		/*
+		 * Initial try using byte code:
+		 * 	        byte[] arrOfBytes  = s.getBytes();
+         *			Boolean prevWasInt = false;
+         *			Boolean prevWasE   = false;
+         *
+ 		 *	        for (byte b : arrOfBytes){
+ 		 *	            if (b >= 48 && b <= 57){
+		 *	                prevWasInt = true;
+		 *	                return true;
+         *			}
+		 */
+		
 	}
 
 }
